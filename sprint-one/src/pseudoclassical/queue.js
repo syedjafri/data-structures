6f89b49c1d5612540ffe85bd28a1458837e55da3
Syed Jafri <syedjafri@gmail.com>
2015-07-02 19:38:51 -0400

    Implemented Queue pseudoclassical methods

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // use keyword this, to store
+  // storage, key, nextValue
+  this.storage = {};
+  this.key = 0;
+  this.nextValue = 0;
 };
+
+Queue.prototype = {
+  constructor: Queue, 
+  enqueue: function(value) {
+   this.storage[this.key++] = value;
+  },
+  dequeue: function(){
+    var result = this.storage[this.nextValue];
+    delete this.storage[this.nextValue++];
+    return result;
+  },
+  size: function(){
+    return Object.keys(this.storage).length;
+  }  
+}
+
+// create prototype object for Queue
+// add constructor Queue, size, push and pop methods
+
 
 
