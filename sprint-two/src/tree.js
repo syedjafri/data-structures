b7839aa755d571df4ae64f5fea11162444cf160f
Syed Jafri <syedjafri@gmail.com>
2015-07-03 15:16:21 -0400

    Implemented Trees

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,8 +3,9 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  // return keys of newTree object
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -15,14 +16,23 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(new Tree(value));
 };
 
 treeMethods.contains = function(target){
+  // if tree value is equal to target, return true
+  if (this.value === target) { return true; }
+  
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) { return true; } 
+  }
 
+  return false;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addChild: has a complexity of O(1) because it is simply adding to the end of an array.
+ * contains: has a complexity of O(n^2) because it has to recurse down the tree to make sure it hits every value.
  */
