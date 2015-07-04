ca6bc9ca6d49558909d68af30326a1068d2ffad7
Syed Jafri <syedjafri@gmail.com>
2015-07-03 22:04:37 -0400

    Implemented Set Implementation

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,14 +7,31 @@ var Set = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage = this._storage || {};
+  
+  if (!this.contains(item)) { 
+    this._storage[item] = "";
+  }
+  // push string item to set storage - if its not already present
 };
 
 setPrototype.contains = function(item){
+  if (this._storage) {
+    return this._storage.hasOwnProperty(item);
+  }
+  
+  return false;
+  // search storage item for string item
 };
 
 setPrototype.remove = function(item){
+  if (this.contains(item)) {
+    delete this._storage[item];
+  }
+  // remove string from the storage
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * All functions are O(1) due to direct access or utilizing O(1) functions.
  */
