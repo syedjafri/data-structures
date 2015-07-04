f67f02a2375b8e9e4f6cda1596a6a5be94a6dbf2
Syed Jafri <syedjafri@gmail.com>
2015-07-03 23:30:58 -0400

    Implemented Hash Tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,18 +5,35 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //should check that stored item in storage[i] location is a thing
+    //if not a thing storage[i] = LinkedList()
+  
+  if (this._storage[i]===undefined){
+    this._storage[i] = LinkedList();
+   }
+
+    this._storage[i].addToTail(v);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //should return last value added to storage location collection
+  if (!this._storage[i] || !this._storage[i].head) { return null; }
+
+  var result = this._storage[i].head.value;
+  if(this._storage[i].head !== this._storage[i].tail) {
+    this._storage[i].addToTail(this._storage[i].removeHead());
+  }
+  return result;
 
 };
 
 HashTable.prototype.remove = function(k){
-
+  //should get index for key
+  //should remove from storage
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i].removeHead();
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
